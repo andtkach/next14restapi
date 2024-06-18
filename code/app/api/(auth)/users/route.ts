8c2ba6bb68a4b54d1821057bd7a7fefe91e1b2e3
Andrii Tkach <andtkach@gmail.com>
2024-06-18 18:08:31 +0200

    Init code

---
+++ code/app/api/(auth)/users/route.ts
@@ -0,0 +1,125 @@
+import connect from "@/lib/db";
+import User from "@/lib/modals/user";
+import { NextResponse } from "next/server";
+import { Types } from "mongoose";
+
+const ObjectId = require("mongoose").Types.ObjectId;
+
+export const GET = async () => {
+  console.log('GET users...');
+  try {
+    await connect();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error: any) {
+    return new NextResponse("Error in fetching users" + error.message, {
+      status: 500,
+    });
+  }
+};
+
+export const POST = async (request: Request) => {
+  console.log('POST user...');
+  try {
+    const body = await request.json();
+    console.log('User data', body);
+    await connect();
+    const newUser = new User(body);
+    await newUser.save();
+
+    return new NextResponse(
+      JSON.stringify({ message: "User is created", user: newUser }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new NextResponse("Error in creating user" + error.message, {
+      status: 500,
+    });
+  }
+};
+
+export const PATCH = async (request: Request) => {
+  console.log('PATCH user...');
+  try {
+    const body = await request.json();
+    const { userId, name } = body;
+
+    await connect();
+    if (!userId || !name) {
+      return new NextResponse(
+        JSON.stringify({ message: "ID or name not found" }),
+        { status: 400 }
+      );
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid User id" }), {
+        status: 400,
+      });
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: new ObjectId(userId) },
+      { name: name },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return new NextResponse(
+        JSON.stringify({ message: "User not found in the database" }),
+        { status: 400 }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({ message: "User is updated", user: updatedUser }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new NextResponse("Error in updating user" + error.message, {
+      status: 500,
+    });
+  }
+};
+
+export const DELETE = async (request: Request) => {
+  console.log('DELETE user...');
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+    console.log('userId', userId);
+    if (!userId) {
+      return new NextResponse(JSON.stringify({ message: "ID not found" }), {
+        status: 400,
+      });
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid User id" }), {
+        status: 400,
+      });
+    }
+
+    await connect();
+
+    const deletedUser = await User.findByIdAndDelete(
+      new Types.ObjectId(userId)
+    );
+
+    if (!deletedUser) {
+      return new NextResponse(
+        JSON.stringify({ message: "User not found in the database" }),
+        { status: 400 }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({ message: "User is deleted", user: deletedUser }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new NextResponse("Error in deleting user" + error.message, {
+      status: 500,
+    });
+  }
+};
